7d5bb45005c345dde4fdec8050319528da968d8a
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-16 15:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const table=document.getElementById("sampleTable");
+console.log(table.childNodes[1].childNodes[0]);
 function insert_Row() {
-    //Write your code here
-  
-  
+  const tr=document.createElement("tr");
+	const td1=document.createElement("td");
+	td1.innerText="New Cell1"
+	const td2=document.createElement("td");
+	td2.innerText="New Cell2"
+	tr.append(td1,td2);
+    table.insertBefore(tr,table.childNodes[1]);
 }
